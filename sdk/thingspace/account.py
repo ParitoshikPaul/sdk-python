c3d7b0efd88748fc102ca1bfb124d40802ca0f6b
Ravinder Singh <Ravinder.Singh@synchronoss.com>
2016-10-25 18:14:41 +0530

    Added templates and accounts functionality

---
+++ sdk/thingspace/account.py
@@ -0,0 +1,16 @@
+from api import cloud
+from RESTClient import RESTClient
+
+class account(cloud):
+
+    def __init__(self, access_token, destination_url):
+        cloud.__init__(self)
+        self.access_token = access_token
+        self.destination_url = destination_url
+        self.rest_client = RESTClient()
+
+    def getaccount(self):
+        return self.rest_client.get(self.destination_url, {}, {'Authorization':  'Bearer ' +
+                                                   self.access_token})
+
+

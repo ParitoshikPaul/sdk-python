a3e71497fd7d8bd7e36a041915e4bb39d6ad5764
Ravinder Singh <Ravinder.Singh@synchronoss.com>
2016-10-28 22:45:48 +0530

    Bugfixes + sample code

--- sample-app/kepler/explorer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^files', views.files, name='files'),
-    url(r'^explorer', views.explorer, name='explorer'),
-]

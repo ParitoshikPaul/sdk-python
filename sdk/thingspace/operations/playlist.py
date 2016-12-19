5afdebf37f86bc76ec99c810e5f088cb78681281
Paritoshik Paul <ParitoshikPaul@2646883.no-reply.drupal.org>
2016-12-19 19:37:35 +0530

    Implemented Delete Api calls

--- sdk/thingspace/operations/playlist.py
+++ sdk/thingspace/operations/playlist.py
@@ -1,8 +1,6 @@
 from thingspace.env import Env
 from thingspace.packages.requests.requests import Request
-from thingspace.utils.path import Path
-from thingspace.models.override import Override
-
+from thingspace.exceptions import CloudError, NotFoundError, ConflictError
 
 
 class Playlists():
@@ -29,7 +27,7 @@ class Playlists():
         playlist = resp.json()
         return playlist
 
-    def get_playlist(self, playlistUid=''):
+    def get_playlist_definition(self, playlistUid=''):
         if not self.authenticated:
             return None
 
@@ -66,6 +64,21 @@ class Playlists():
         playlist_items = resp.json()
         return playlist_items
 
+    def get_playlist_items_content(self, playlistUid='', item_uid=''):
+        if not self.authenticated:
+            return None
+
+        headers = {
+            "Authorization": "Bearer " + self.access_token
+        }
+        resp = self.networker(Request(
+            'GET',
+            Env.api_url + '/cloud/' + Env.api_version + '/playlists/' + playlistUid + '/items' + item_uid,
+            headers=headers
+        ))
+
+        playlist_items_content = resp.json()
+        return playlist_items_content
 
     def create_playlist(self, name="", paths="", type=""):
 
@@ -88,4 +101,40 @@ class Playlists():
 
         json = resp.json()
         return json
+
+    def delete_playlist(self, playlist_uid=""):
+
+        resp = self.networker(Request(
+            'DELETE',
+            Env.api_cloud + '/playlists/' + playlist_uid,
+            headers={
+                "Authorization": "Bearer " + self.access_token
+            }
+        ))
+
+        if resp.status_code == 404:
+            raise NotFoundError("Playlist not found", response=resp)
+
+        if resp.status_code != 200:
+            raise CloudError("Could not delete playlist", response=resp)
+
+            return
+
+    def delete_playlist_item(self, playlist_uid="", item_uid=""):
+
+        resp = self.networker(Request(
+            'DELETE',
+            Env.api_cloud + '/playlists/' + playlist_uid + '/items/' + item_uid,
+            headers={
+                "Authorization": "Bearer " + self.access_token
+            }
+        ))
+
+        if resp.status_code == 404:
+            raise NotFoundError("Playlist Item not found", response=resp)
+
+        if resp.status_code != 200:
+            raise CloudError("Could not delete playlist item", response=resp)
+
+            return
 
